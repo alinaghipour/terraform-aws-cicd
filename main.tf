190f84dd70872d303054d7163650c1a47cedd37b
alinaghipour <44332914+alinaghipour@users.noreply.github.com>
2021-03-26 14:11:58 +0100

    Update main.tf
    change s3 access for access codebuild to that

--- main.tf
+++ main.tf
@@ -106,10 +106,13 @@ data "aws_iam_policy_document" "s3" {
     sid = ""
 
     actions = [
-      "s3:GetObject",
-      "s3:GetObjectVersion",
-      "s3:GetBucketVersioning",
       "s3:PutObject",
+      "s3:GetObject",
+      "s3:DeleteObject",
+      "s3:PutObjectAcl",
+      "s3:GetObjectVersion",
+      "s3:GetBucketAcl",
+      "s3:GetBucketLocation",
     ]
 
     resources = [
